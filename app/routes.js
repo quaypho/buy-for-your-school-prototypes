e6fcfeeb70d9210302f12d8c6147809d609c40c6
F <f@dxw.com>
2020-09-02 17:52:15 +0100

    Render questions from Contentful based on their slug
    We let Contentful drive the flow. It uses `unmanagedPage`s to indicate
    when it should hand off to a hardcoded page (like a check your answers
    page).

--- app/routes.js
+++ app/routes.js
@@ -23,4 +23,44 @@ router.post("/sprint-five/resume-or-new", function(req, res) {
   }
 });
 
+router.get("/contentful-test/:slug", async (req, res) => {
+  const questionEntries = await contentfulClient.getEntries({
+    content_type: "question",
+    include: 2
+  });
+  const question = questionEntries.items.find(
+    (entry) => entry.fields.slug === `/${req.params.slug}`
+  );
+
+  if (!question) {
+    const pageEntries = await contentfulClient.getEntries({
+      content_type: "unmanagedPage",
+    });
+    const page = pageEntries.items.find(
+      (entry) => entry.fields.slug === `/${req.params.slug}`
+    );
+
+    if (!page) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.render(`contentful-test${page.fields.slug}`);
+    return;
+  }
+
+  const { title, helpText } = question.fields;
+
+  const options = question.fields.options.map((option) => ({
+    value: option.fields.next.fields.slug,
+    text: option.fields.label,
+  }));
+
+  res.render("contentful-test/question", { title, helpText, options });
+});
+
+router.post("/contentful-test/answer", (req, res) => {
+  res.redirect(`/contentful-test${req.body["next-slug"]}`);
+});
+
 module.exports = router
