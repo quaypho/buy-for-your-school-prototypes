1e813fa92f5b112a084b852d7b2b738fa4867fe1
Rich Cooley <rich@dxw.com>
2020-09-02 12:10:30 +0100

    Set up inital flow without full email recovery

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get("/sprint-five/resume-or-new", function(req, res) {
+  res.render("sprint-five/resume-or-new");
+});
+
+router.post("/sprint-five/resume-or-new", function(req, res) {
+  let resumeExisting = req.session.data["resume-existing"];
+
+  if (resumeExisting === "Yes - resume my existing procurement") {
+    res.redirect("/sprint-five/resume-email");
+  } else {
+    res.redirect("/sprint-four/step-by-step");
+  }
+});
+
 module.exports = router
