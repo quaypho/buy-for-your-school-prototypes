791fd8b7f574eef123e825d94648370614ffaec7
Pete Blakemore <contact@peteblakemore.co.uk>
2020-11-26 13:54:46 +0000

    Created beta area

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Buy for your school: step by step',
+  serviceName: 'Buy for your school',
 
   // Default port that prototype runs on
   port: '3000',
